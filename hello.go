5c8c5550ed2f00ff9f44dacce6c0103208dba10b
quyidong <quyidong@gmail.com>
2016-06-13 15:48:22 +0800

    test

--- hello.go
+++ hello.go
@@ -1,8 +1,16 @@
 package main
 
 import "fmt"
+import "net"
 
 func main(){
 	fmt.Printf("Hello,World\n中文输出\n")
-	fmt.Printf("たなか")
+	fmt.Printf("たなか\n")
+	if ln,err :=net.Listen("tcp","7892");err ==nil{
+		defer ln.Close()
+		for{
+			ln.Accept()
+			fmt.Println("Receive a Message")
+		}
+	}
 }
